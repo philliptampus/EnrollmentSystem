6aba98f03d8729c9bc7129d2f2116a1a1041deac
Lisa Lewis <mrs.lisalewis@outlook.com>
2023-02-19 13:08:30 +0800

    Update on Connecting API to Database

--- EnrollmentSystem/Controllers/AdminController.cs
+++ EnrollmentSystem/Controllers/AdminController.cs
@@ -19,35 +19,108 @@ namespace EnrollmentSystem.Controllers
 
         [AllowAnonymous]
         [HttpGet("GetUsers")]
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<IEnumerable<Users>> GetUsers()
         {
 
-            string query = $@"SELECT * FROM [user];";
+            string query = $@"SELECT * FROM [users];";
 
-            var result = await _baseAccessLayer.QueryListAsync<User>(query, null, CommandType.Text);
+            var result = await _baseAccessLayer.QueryListAsync<Users>(query, null, CommandType.Text);
+
+            return result;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("GetStudent")]
+        public async Task<IEnumerable<Student>> GetStudent()
+        {
+
+            string query = $@"SELECT * FROM Student;";
+
+            var result = await _baseAccessLayer.QueryListAsync<Student>(query, null, CommandType.Text);
+
+            return result;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("GetCourse")]
+        public async Task<IEnumerable<Course>> GetCourse()
+        {
+
+            string query = $@"SELECT * FROM Course;";
+
+            var result = await _baseAccessLayer.QueryListAsync<Course>(query, null, CommandType.Text);
 
             return result;
         }
 
 
         [AllowAnonymous]
-        [HttpPost("User")]
-        public async Task<User> SaveUser([FromBody] User user)
+        [HttpPost("Users")]
+        public async Task<Users> SaveUser([FromBody] Users user)
         {
-            var query = _baseAccessLayer.GenerateInsertQuery<User>("user", user, "Id");
+            var query = _baseAccessLayer.GenerateInsertQuery<Users>("user", user, "Id");
             var result = await _baseAccessLayer.ExecuteNonQueryAsync(query, user, commandType: CommandType.Text);
 
             return user;
         }
 
         [AllowAnonymous]
+        [HttpPost("Student")]
+        public async Task<Student> SaveStudent([FromBody] Student student)
+        {
+            var query = _baseAccessLayer.GenerateInsertQuery<Student>("Student", student, "Id");
+            var result = await _baseAccessLayer.ExecuteNonQueryAsync(query, student, commandType: CommandType.Text);
+
+            return student;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Course")]
+        public async Task<Course> SaveCourse([FromBody] Course course)
+        {
+            var query = _baseAccessLayer.GenerateInsertQuery<Course>("Course", course, "Id");
+            var result = await _baseAccessLayer.ExecuteNonQueryAsync(query, course, commandType: CommandType.Text);
+
+            return course;
+        }
+
+        [AllowAnonymous]
         [HttpGet("User/{id}")]
-        public async Task<User> GetUser(int id)
+        public async Task<Users> GetUser(int id)
         {
             string query = $@"SELECT * FROM [user] WHERE Id=@Id;";
-            var result = await _baseAccessLayer.QuerySingleOrDefaultAsync<User>(query, new { Id = id }, commandType: CommandType.Text);
+            var result = await _baseAccessLayer.QuerySingleOrDefaultAsync<Users>(query, new { Id = id }, commandType: CommandType.Text);
+
+            return result;
+
+        }
+        [AllowAnonymous]
+        [HttpGet("Student/{id}")]
+        public async Task<Student> GetStudent(int id)
+        {
+            string query = $@"SELECT * FROM Student WHERE Id=@Id;";
+            var result = await _baseAccessLayer.QuerySingleOrDefaultAsync<Student>(query, new { Id = id }, commandType: CommandType.Text);
 
             return result;
         }
+        [AllowAnonymous]
+        [HttpGet("Course/{id}")]
+        public async Task<Course> GetCourse(int id)
+            {
+                string query = $@"SELECT * FROM Course WHERE Id=@Id;";
+                var result = await _baseAccessLayer.QuerySingleOrDefaultAsync<Course>(query, new { Id = id }, commandType: CommandType.Text);
+
+                return result;
+            }
     }
 }
+
+
+
+
+/*CRUD                                                        HTTP VERBS
+ * CREATE - a new record                                    - POST
+ * READ   - retrieve a single/list of record(s)             - GET
+ * UPDATE - modify an existing record                       - PUT / PATCH
+ * DELETE - remove an existing record                       - DELETE
+*/
--- EnrollmentSystem/Controllers/StudentController.cs
+++ EnrollmentSystem/Controllers/StudentController.cs
@@ -1,4 +1,4 @@
-using EnrollmentSystem.Models;
+﻿using EnrollmentSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EnrollmentSystem.Controllers
@@ -31,8 +31,126 @@ namespace EnrollmentSystem.Controllers
             .ToArray();
 
 
-           
+
 
         }
     }
 }
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EnrollmentSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using TicketingAPI.Dapper;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using NuGet.Packaging.Signing;
+
+namespace EnrollmentSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly APIDbContext _context;
+
+        public StudentController(APIDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Student
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Users>>> GetStudent1()
+        {
+            return await _context.Student1.ToListAsync();
+        }
+
+        // GET: api/Student/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUsers(int id)
+        {
+            var users = await _context.Student1.FindAsync(id);
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return users;
+        }
+
+        // PUT: api/Student/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsers(int id, Student users)
+        {
+            if (id != users.Student_Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(users).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Student
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        
+
+
+        [HttpPost]
+        [Route ("addstudent")]
+        [AllowAnonymous]
+
+       public async Task<ActionResult<Users>> PostUsers(Users users)
+        { 
+            _context.Student1.Add(users);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsers", new { id = users.Student_Id }, users);
+        }
+
+        // DELETE: api/Student/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsers(int id)
+        {
+            var users = await _context.Student1.FindAsync(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            _context.Student1.Remove(users);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UsersExists(int id)
+        {
+            return _context.Student1.Any(e => e.Student_Id == id);
+        }
+    }
+}
+*/
